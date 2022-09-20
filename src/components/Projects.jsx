29eac67b2feb4a5cb9f5aef237f427834e89def2
Luiz Felipe <lfar05071@gmail.com>
2022-09-20 14:37:32 -0300

    portifolio-responsive

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,7 +4,7 @@ const Projects = () => {
   return (
     <section className='projects'>
         <h2>Projetos</h2>
-        <p>Já produzir muitos projetos de diferentes áreas da programação como: Web, Mineração de Dados, Mobile, Desktop.</p>
+        <p>Já produzi muitos projetos de diferentes áreas da programação como: Web, Mineração de Dados, Mobile, Desktop.</p>
         <a href="https://github.com/LuizFelipe33?tab=repositories" target="_blank" className='btn'>
             Ver Projetos
         </a>
